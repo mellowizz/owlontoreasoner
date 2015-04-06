712499725e48dd0ccb27a7aa7d456c1f5b7f0a23
Moran <niklasmoran@mailbox.tu-berlin.de>
2015-04-06 19:37:47 +0200

    too many null values. DOES NOT WORK

---
+++ rlp_onto_reasoner/src/JDBCPostgis/Database.java
@@ -0,0 +1,13 @@
+package JDBCPostgis;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class Database {
+
+}
--- rlp_onto_reasoner/src/test/testReasoner.java
+++ rlp_onto_reasoner/src/test/testReasoner.java
@@ -1,9 +1,16 @@
 package test;
 
 import java.io.File;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map.Entry;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 import org.semanticweb.owlapi.reasoner.OWLReasoner;
 import org.semanticweb.owlapi.reasoner.OWLReasonerFactory;
@@ -20,20 +27,71 @@ import uk.ac.manchester.cs.factplusplus.owlapiv3.FaCTPlusPlusReasonerFactory;
 import ontology.ReasonerHermit;
 
 public class testReasoner {
-	public static void main(String[] args){
+	public static void createTable(HashMap<String, ArrayList<String>> myMap) {
+		// TODO Auto-generated method stub
+		Connection con = null;
+		Statement st = null;
+		ResultSet rs = null;
+		String url = "jdbc:postgresql://localhost/RLP";
+		String user = "postgres";
+		String password = "BobtheBuilder";
+		try {
+			con = DriverManager.getConnection(url, user, password);
+			con.setAutoCommit(false);
+			st = con.createStatement();
+			//st.executeQuery("'rlp_results'");
+			st.addBatch("CREATE TABLE rlp_results(ogc_fid VARCHAR(25), water_regime VARCHAR(25), the_geom geometry(MultiPolygon,25832))");
+			for (Entry<String, ArrayList<String>> ee : myMap.entrySet()) {
+				String key = ee.getKey();
+				ArrayList<String> values = ee.getValue();
+				System.out.println();
+				System.out.println(key + ":");
+				for (String i : values) {
+					String query = "insert into rlp_results values('" + i
+							+ "','" + key + "')";
+					System.out.println(query);
+					st.addBatch(query);
+				}
+			}
+			st.executeBatch();
+			con.commit();
+			
+			//System.out.println("Commited: "+ counts.lenth + " successfully!");
+
+		} catch (SQLException ex) {
+			Logger lgr = Logger.getLogger(testReasoner.class.getName());
+			lgr.log(Level.SEVERE, ex.getMessage(), ex);
+		} finally {
+			try {
+				if (rs != null) {
+					rs.close();
+				}
+				if (st != null) {
+					st.close();
+				}
+				if (con != null) {
+					con.close();
+				}
+			} catch (SQLException ex) {
+				Logger lgr = Logger.getLogger(testReasoner.class.getName());
+				lgr.log(Level.SEVERE, ex.getMessage(), ex);
+			}
+		}
+	}
+
+	public static void main(String[] args) {
 		OWLOntologyManager mgr = OWLManager.createOWLOntologyManager();
-		File file = new File("C:\\Users\\Moran\\ontologies\\EUNIS_ext_small.owl");
+		File file = new File(
+				"C:\\Users\\Moran\\ontologies\\EUNIS_ext_small.owl");
 		OWLOntology onto = null;
-		if( mgr == null || file == null){
+		if (mgr == null || file == null) {
 			System.out.println("ERROR!!");
 		}
 		System.out.println("Before try");
-		try{
+		try {
 			onto = mgr.loadOntologyFromOntologyDocument(file);
-			//Reasoner hermit = new Reasoner(onto);
-			//OWLReasoner hermit = ReasonerHermit.createOWLReasoner(onto);
-			OWLReasoner factplusplus = new FaCTPlusPlusReasonerFactory().createReasoner(onto);
-			//System.out.println(hermit.getReasonerVersion());
+			OWLReasoner factplusplus = new FaCTPlusPlusReasonerFactory()
+					.createReasoner(onto);
 			System.out.println(factplusplus.getReasonerVersion());
 			HashMap<String, ArrayList<String>> classesHash = new HashMap<String, ArrayList<String>>();
 			classesHash.put("waterlogged", new ArrayList<String>());
@@ -46,43 +104,30 @@ public class testReasoner {
 			classList.add("periodic_flooding");
 			classList.add("riparian");
 			classList.add("dry_or_seasonally_wet");
-			for (OWLClass c : onto.getClassesInSignature()){
-				// dry_or_seasonally wet=0, periodific_flooding=0?
-				/*String currClass = c.getIRI().getFragment();
-				if (classesHash.get(currClass) == null){
-					classesHash.put(c.getIRI().getFragment(), new ArrayList<Integer>());
-				}
-				*/
-				if (classList.isEmpty()) break;
-				if (c.getIRI().getFragment().equals(classList.remove(0))){
+			for (OWLClass c : onto.getClassesInSignature()) {
+				if (classList.isEmpty())
+					break;
+				if (c.getIRI().getFragment().equals(classList.remove(0))) {
 					String currClass = c.getIRI().getFragment();
 					System.out.println("CurrClass:" + currClass);
-					//NodeSet<OWLNamedIndividual>instances = hermit.getInstances(c, false);
-					NodeSet<OWLNamedIndividual>instances = factplusplus.getInstances(c, false);
-					//System.out.println(instances.toString());
-					//System.out.println(c.getIRI().getFragment());
-					//instances.
+					NodeSet<OWLNamedIndividual> instances = factplusplus
+							.getInstances(c, false);
 					for (OWLNamedIndividual i : instances.getFlattened()) {
-						//classesHash.get(currClass, )	
-						classesHash.get(currClass).add(i.getIRI().getFragment());
+						// classesHash.get(currClass, )
+						classesHash.get(currClass)
+								.add(i.getIRI().getFragment());
 						System.out.println(i.getIRI().getFragment());
+
 					}
-					System.out.println("Total: " + instances.getFlattened().size());
+					System.out.println("Total: "
+							+ instances.getFlattened().size());
 				}
 			}
-			for (Entry<String, ArrayList<String>> ee: classesHash.entrySet()){
-				String key = ee.getKey();
-				ArrayList<String> values = ee.getValue();
-				System.out.println();
-				System.out.println(key + ":");
-				for (String i : values){
-					System.out.println(i);
-				}
-			}
+			createTable(classesHash);
 		} catch (OWLOntologyCreationException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
-		} finally{ 
+		} finally {
 			System.out.println("STUB:");
 		}
 	}
