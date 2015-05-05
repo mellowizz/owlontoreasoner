d409e1c10cc30a5315abb5b47219e69205edade5
Moran <niklasmoran@mailbox.tu-berlin.de>
2015-05-05 15:44:47 +0200

    has both Hermit and factplusplus as reasoners.

--- rlp_onto_reasoner/src/test/testReasoner.java
+++ rlp_onto_reasoner/src/test/testReasoner.java
@@ -9,26 +9,44 @@ import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map.Entry;
+import java.util.Set;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
+import org.semanticweb.owlapi.reasoner.InferenceType;
 import org.semanticweb.owlapi.reasoner.OWLReasoner;
 import org.semanticweb.owlapi.reasoner.OWLReasonerFactory;
 import org.semanticweb.HermiT.Reasoner;
 import org.semanticweb.owlapi.apibinding.OWLManager;
+import org.semanticweb.owlapi.model.OWLAnnotation;
+import org.semanticweb.owlapi.model.OWLAnnotationObjectVisitorEx;
+import org.semanticweb.owlapi.model.OWLAnonymousIndividual;
 import org.semanticweb.owlapi.model.OWLClass;
+import org.semanticweb.owlapi.model.OWLEntity;
+import org.semanticweb.owlapi.model.OWLLiteral;
 import org.semanticweb.owlapi.model.OWLNamedIndividual;
+import org.semanticweb.owlapi.model.OWLObjectProperty;
 import org.semanticweb.owlapi.model.OWLOntology;
 import org.semanticweb.owlapi.model.OWLOntologyCreationException;
 import org.semanticweb.owlapi.model.OWLOntologyManager;
 import org.semanticweb.owlapi.reasoner.NodeSet;
+import org.semanticweb.owlapi.util.OWLObjectVisitorExAdapter;
 
 import uk.ac.manchester.cs.factplusplus.owlapiv3.FaCTPlusPlusReasonerFactory;
 import ontology.ReasonerHermit;
 
+class LabelExtractor extends OWLObjectVisitorExAdapter<String> implements OWLAnnotationObjectVisitorEx<String> {
+	public String visit(OWLAnnotation annotation) { 
+		if (annotation.getProperty().isLabel()) {
+			OWLLiteral c = (OWLLiteral) annotation.getValue();
+		return c.getLiteral();
+		}
+		return null;
+	}
+}
+
 public class testReasoner {
 	public static void createTable(HashMap<String, ArrayList<String>> myMap) {
-		// TODO Auto-generated method stub
 		Connection con = null;
 		Statement st = null;
 		ResultSet rs = null;
@@ -55,8 +73,9 @@ public class testReasoner {
 			}
 			st.executeBatch();
 			con.commit();
-			
-			//System.out.println("Commited: "+ counts.lenth + " successfully!");
+
+			// System.out.println("Commited: "+ counts.lenth +
+			// " successfully!");
 
 		} catch (SQLException ex) {
 			Logger lgr = Logger.getLogger(testReasoner.class.getName());
@@ -79,45 +98,77 @@ public class testReasoner {
 		}
 	}
 
+	private static LabelExtractor le = new LabelExtractor();
+
+	private static String labelFor(OWLEntity clazz, OWLOntology o){
+		Set<OWLAnnotation> annotations = clazz.getAnnotations(o);
+		for (OWLAnnotation anno: annotations){
+			String result = anno.accept(le);
+			if (result != null){
+				return result;
+			}
+		}
+		return clazz.getIRI().toString();
+	}
+	
 	public static void main(String[] args) {
 		OWLOntologyManager mgr = OWLManager.createOWLOntologyManager();
 		File file = new File(
-				"C:\\Users\\Moran\\ontologies\\EUNIS_ext_small.owl");
+				"C:\\Users\\Moran\\ontologies\\wetness_base.owl");
 		OWLOntology onto = null;
 		if (mgr == null || file == null) {
 			System.out.println("ERROR!!");
 		}
+		HashMap<OWLClass,Set<OWLClass>> someValueFromAxioms = new HashMap<OWLClass,Set<OWLClass>>();
+
 		System.out.println("Before try");
 		try {
 			onto = mgr.loadOntologyFromOntologyDocument(file);
-			OWLReasoner factplusplus = new FaCTPlusPlusReasonerFactory()
-					.createReasoner(onto);
-			System.out.println(factplusplus.getReasonerVersion());
+			OWLReasoner hermit = new Reasoner.ReasonerFactory().createReasoner(onto);
+			/*OWLReasoner factplusplus = new FaCTPlusPlusReasonerFactory()
+					.createReasoner(onto); */
+			//System.out.println(factplusplus.getReasonerVersion());
+			//factplusplus.precomputeInferences(InferenceType.CLASS_HIERARCHY);
+			System.out.println(hermit.getReasonerVersion());
 			HashMap<String, ArrayList<String>> classesHash = new HashMap<String, ArrayList<String>>();
-			classesHash.put("waterlogged", new ArrayList<String>());
+			/*classesHash.put("waterlogged", new ArrayList<String>());
 			classesHash.put("periodic_flooding", new ArrayList<String>());
 			classesHash.put("riparian", new ArrayList<String>());
 			classesHash.put("dry_or_seasonally_wet", new ArrayList<String>());
-			// convert nodeset to lis?
+			*/
+			classesHash.put("aquatic", new ArrayList<String>());
+			classesHash.put("dry", new ArrayList<String>());
+			classesHash.put("moist", new ArrayList<String>());
+			classesHash.put("mesic", new ArrayList<String>());
+			classesHash.put("wet", new ArrayList<String>());
 			ArrayList<String> classList = new ArrayList<String>();
-			classList.add("waterlogged");
+			classList.add("aquatic");
+			classList.add("dry");
+			classList.add("moist");
+			classList.add("mesic");
+			classList.add("wet");
+			/*classList.add("waterlogged");
 			classList.add("periodic_flooding");
 			classList.add("riparian");
-			classList.add("dry_or_seasonally_wet");
+			classList.add("dry_or_seasonally_wet");*/
+			
 			for (OWLClass c : onto.getClassesInSignature()) {
-				if (classList.isEmpty())
+				if (classList.isEmpty()){
+					System.out.println("class list empty!");
 					break;
+				}
 				if (c.getIRI().getFragment().equals(classList.remove(0))) {
 					String currClass = c.getIRI().getFragment();
 					System.out.println("CurrClass:" + currClass);
-					NodeSet<OWLNamedIndividual> instances = factplusplus
+					//NodeSet<OWLNamedIndividual> instances = factplusplus
+					//		.getInstances(c, false);
+					NodeSet<OWLNamedIndividual> instances = hermit
 							.getInstances(c, false);
 					for (OWLNamedIndividual i : instances.getFlattened()) {
 						// classesHash.get(currClass, )
 						classesHash.get(currClass)
 								.add(i.getIRI().getFragment());
 						System.out.println(i.getIRI().getFragment());
-
 					}
 					System.out.println("Total: "
 							+ instances.getFlattened().size());
@@ -128,7 +179,19 @@ public class testReasoner {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		} finally {
-			System.out.println("STUB:");
+			System.out.println("ALL DONE!");
 		}
 	}
 }
+/*
+				for (OWLNamedIndividual i : factplusplus.getInstances(c, false).getFlattened()) {
+					System.out.println(labelFor(i, onto) + labelFor(c, onto));
+					// get assertions:
+					for (OWLObjectProperty op: onto.getObjectPropertiesInSignature()){
+						NodeSet<OWLNamedIndividual> valuesNodeSet = factplusplus.getObjectPropertyValues(i, op);
+						for (OWLNamedIndividual value : valuesNodeSet.getFlattened()){
+							System.out.println(value.getIRI());
+							//classesHash.get(currClass).add(i.getIRI().getFragment());
+							System.out.println(i.getIRI().getFragment());
+						}
+					} */
