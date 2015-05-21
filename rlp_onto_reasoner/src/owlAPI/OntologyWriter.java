95087d43670c69d95bfc168bd009cf7f18057d1d
Moran <niklasmoran@mailbox.tu-berlin.de>
2015-05-21 15:31:29 +0200

    bug in last commit. Runs and reasoning works. ** only 4 rules!

--- rlp_onto_reasoner/src/owlAPI/OntologyWriter.java
+++ rlp_onto_reasoner/src/owlAPI/OntologyWriter.java
@@ -1,6 +1,10 @@
 package owlAPI;
 
+import java.util.HashMap;
 import java.util.LinkedHashSet;
+import java.util.List;
+import java.util.Map.Entry;
+import java.util.Set;
 
 import org.semanticweb.owlapi.apibinding.OWLManager;
 import org.semanticweb.owlapi.model.AddAxiom;
@@ -14,6 +18,7 @@ import org.semanticweb.owlapi.model.OWLDataPropertyAssertionAxiom;
 import org.semanticweb.owlapi.model.OWLDatatype;
 import org.semanticweb.owlapi.model.OWLLiteral;
 import org.semanticweb.owlapi.model.OWLNamedIndividual;
+import org.semanticweb.owlapi.model.OWLObjectIntersectionOf;
 import org.semanticweb.owlapi.model.OWLOntology;
 import org.semanticweb.owlapi.model.OWLOntologyCreationException;
 import org.semanticweb.owlapi.model.OWLOntologyManager;
@@ -23,6 +28,11 @@ import org.semanticweb.owlapi.util.DefaultPrefixManager;
 import org.semanticweb.owlapi.util.SimpleIRIMapper;
 import org.semanticweb.owlapi.vocab.OWL2Datatype;
 
+
+
+
+
+import dict.defaultDict;
 //import conversion.OntologyClass;
 import owlAPI.OntologyClass;
 
@@ -31,15 +41,86 @@ public class OntologyWriter {
 	 * 
 	 * Writes properties and classes to ontology
 	 */
-	
-	OWLOntologyManager manager = OWLManager.createOWLOntologyManager();
-	OWLOntology ontology;
-	OWLDataFactory factory; // = manager.getOWLDataFactory();
-	public OntologyWriter(IRI documentIRI) throws OWLOntologyCreationException{
-		manager.loadOntologyFromOntologyDocument(documentIRI);	
-		factory = manager.getOWLDataFactory();
+
+	/*
+	 * OWLOntologyManager manager = OWLManager.createOWLOntologyManager();
+	 * OWLOntology ontology; OWLDataFactory factory; // =
+	 * manager.getOWLDataFactory(); public OntologyWriter(IRI documentIRI)
+	 * throws OWLOntologyCreationException{
+	 * manager.loadOntologyFromOntologyDocument(documentIRI); factory =
+	 * manager.getOWLDataFactory(); }
+	 */
+
+	public void writeAll(LinkedHashSet<OntologyClass> classes,
+			LinkedHashSet<Individual> individuals, defaultDict<String, List<Set>> classesExpressions, IRI documentIRI,
+			IRI ontologyIRI) throws OWLOntologyCreationException,
+			OWLOntologyStorageException {
+
+		OWLOntologyManager manager = OWLManager.createOWLOntologyManager();
+		OWLOntology ontology = manager
+				.loadOntologyFromOntologyDocument(documentIRI);
+		OWLDataFactory factory = manager.getOWLDataFactory();
+		SimpleIRIMapper mapper = new SimpleIRIMapper(ontologyIRI, documentIRI);
+		manager.addIRIMapper(mapper);
+		PrefixManager pm = new DefaultPrefixManager(ontologyIRI.toString()); // +
+																				// '#'
+
+		OWLClass wetness = factory.getOWLClass(IRI.create(ontologyIRI
+				+ "#wetness"));
+		;
+		for (OntologyClass EUClass : classes) {
+			OWLClass cls = factory.getOWLClass(IRI.create(ontologyIRI + "#"
+					+ EUClass.getName()));
+			OWLClass thing = factory.getOWLThing();
+			OWLAxiom classAx = factory.getOWLSubClassOfAxiom(cls, wetness);
+			OWLAxiom wetnessAx = factory.getOWLSubClassOfAxiom(wetness, thing);
+			manager.applyChange(new AddAxiom(ontology, classAx));
+			manager.applyChange(new AddAxiom(ontology, wetnessAx));
+		}
+
+		System.out.println("# of individuals: " + individuals.size());
+
+		for (Individual ind : individuals) {
+			Integer index = 0;
+
+			OWLNamedIndividual obj = factory.getOWLNamedIndividual(
+					"#" + ind.getFID(), pm);
+
+			for (Number value : ind.getValues()) {
+				OWLDataProperty dataProp = factory.getOWLDataProperty("#"
+						+ ind.getDataPropertyNames().get(index), pm);
+
+				OWLDatatype integerDatatype = factory
+						.getOWLDatatype(OWL2Datatype.XSD_DOUBLE.getIRI());
+
+				OWLLiteral literal = factory.getOWLLiteral(value.toString(),
+						integerDatatype);
+
+				OWLDataPropertyAssertionAxiom dataPropertyAssertion = factory
+						.getOWLDataPropertyAssertionAxiom(dataProp, obj,
+								literal);
+
+				manager.addAxiom(ontology, dataPropertyAssertion);
+
+				index = index + 1;
+			}
+		}
+		OWLObjectIntersectionOf intersection = null;
+		OWLClass owlCls = null;
+		for (Entry<String, List<Set>> entry : classesExpressions.entrySet()) {
+			String currCls = entry.getKey();
+			List<Set> value = entry.getValue();
+			for (Set rule : value){
+				owlCls = factory.getOWLClass(IRI.create("#" + currCls));
+				intersection = factory
+					.getOWLObjectIntersectionOf(rule);
+			}
+			manager.addAxiom(ontology, factory.getOWLEquivalentClassesAxiom(
+					owlCls, intersection));
+		}
+		manager.saveOntology(ontology);
 	}
-	
+
 	public void writeClasses(LinkedHashSet<OntologyClass> classes,
 			IRI documentIRI, IRI ontologyIRI)
 			throws OWLOntologyCreationException, OWLOntologyStorageException {
@@ -56,23 +137,29 @@ public class OntologyWriter {
 
 		SimpleIRIMapper mapper = new SimpleIRIMapper(ontologyIRI, documentIRI);
 		manager.addIRIMapper(mapper);
-		OWLClass wetness = factory.getOWLClass(IRI.create(ontologyIRI + "#wetness")); ;
-		//manager.applyChange(new AddAxiom(ontology, wetnessThing));
+		OWLClass wetness = factory.getOWLClass(IRI.create(ontologyIRI
+				+ "#wetness"));
+		;
+		// manager.applyChange(new AddAxiom(ontology, wetnessThing));
 		for (OntologyClass EUClass : classes) {
-			 OWLClass cls = factory.getOWLClass(IRI.create(ontologyIRI + "#"+EUClass.getName()));
-			 OWLClass thing = factory.getOWLThing();
-			 OWLAxiom classAx = factory.getOWLSubClassOfAxiom(cls, wetness);
-			 manager.applyChange(new AddAxiom(ontology, classAx));
-			 
-			 //OWLAnnotation commentAnno = factory.getOWLAnnotation(factory.getRDFSComment(),
-		     //          factory.getOWLLiteral(EUClass.getDescription(), "en"));
-			 //OWLAxiom ax = factory.getOWLAnnotationAssertionAxiom(cls.getIRI(),
-		     //           commentAnno);
-			 //manager.applyChange(new AddAxiom(ontology, ax));
+			OWLClass cls = factory.getOWLClass(IRI.create(ontologyIRI + "#"
+					+ EUClass.getName()));
+			OWLClass thing = factory.getOWLThing();
+			OWLAxiom classAx = factory.getOWLSubClassOfAxiom(cls, wetness);
+			manager.applyChange(new AddAxiom(ontology, classAx));
+
+			// OWLAnnotation commentAnno =
+			// factory.getOWLAnnotation(factory.getRDFSComment(),
+			// factory.getOWLLiteral(EUClass.getDescription(), "en"));
+			// OWLAxiom ax =
+			// factory.getOWLAnnotationAssertionAxiom(cls.getIRI(),
+			// commentAnno);
+			// manager.applyChange(new AddAxiom(ontology, ax));
 		}
+
 		manager.saveOntology(ontology);
 	}
-	
+
 	public void writeIndividuals(LinkedHashSet<Individual> individuals,
 			IRI documentIRI) throws OWLOntologyCreationException,
 			OWLOntologyStorageException {
@@ -90,10 +177,11 @@ public class OntologyWriter {
 
 		manager.addIRIMapper(mapper);
 
-		PrefixManager pm = new DefaultPrefixManager(ontologyIRI.toString()); // + '#'
-		//OWLClass eunis = factory.getOWLClass(":")
-		//manager.addAxiom(ontology, eunis);
-		
+		PrefixManager pm = new DefaultPrefixManager(ontologyIRI.toString()); // +
+																				// '#'
+		// OWLClass eunis = factory.getOWLClass(":")
+		// manager.addAxiom(ontology, eunis);
+
 		for (Individual ind : individuals) {
 			Integer index = 0;
 
