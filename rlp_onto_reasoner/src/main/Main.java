28fedd9730107c4351489befe966808ffe1ce371
Moran <niklasmoran@mailbox.tu-berlin.de>
2015-10-28 18:37:20 +0100

    a few small changes.

--- rlp_onto_reasoner/src/main/Main.java
+++ rlp_onto_reasoner/src/main/Main.java
@@ -13,25 +13,17 @@ public class Main {
 	public static void main(String[] args) throws SQLException, IOException{
 		RLPUtils.getOsName();
 		// table to classify on
-		String parameter = "NATFLO_homogeneity";
-		String tableName = "testing_natflo_homogeneity_600"; //args[0];
-        String rule = "c:/Users/Moran/test-rlp/scikit-learn_rules/training_natflo_homogeneity_200_cv.csv";
-		int numRules = 8;
+		String parameter = "NATFLO_usage_intensity";
+		String tableName = "testing_" + parameter.toLowerCase() + "_600";
+		//String tableName = "testing_natflo_depression"; //args[0];
+        String rule = "c:/Users/Moran/test-rlp/scikit-learn_rules/training_natflo_usage_intensity_200.csv";
+        //String ruleDir = "c:/Users/Moran/test-rlp/SEaTH/natflo_slope_position_200_seath";
+		int numRules = 10;
 		String algorithm = "dt";
 		String colName = parameter;
 		DBToOWLIndividualConverter test = new DBToOWLIndividualConverter("jdbc:postgresql://localhost:5432/RLP?user=postgres&password=BobtheBuilder",
 				tableName, parameter, rule, colName, numRules, algorithm);
-		// SEaTH rules to use
-		//String rulesDir = "C:/Users/Moran/test-rlp/SEaTH/natflo_wetness_eagle_vegetation_type_1_200";
-		//String rule = "/home/niklasmoran/test-rlp/scikit-learn/training_natflo_usage_intensity_rules.csv";
-		// DT rule to use.
-        //String rule = "c:/Users/Moran/test-rlp/WEKA/testing_natflo_wetness_vegetation_type_1_CART.csv";
-        //String rule = "c:/Users/Moran/tubCloud/Thesis/dt_natflo_slope_rules.csv";
-        //argmin_DT_3_rules.csv";
-		// number of rules to use
-		//System.out.println("About to create OWL file from table: " + tableName);
 		File owlFile = test.convertDB(); //tableName, rule, "dt", numRules, parameter); // , fields);
-		// File owlFile = new File("/home/niklasmoran/ontologies/testing_natflo_wetness_class_name_600_8dt_rules.owl");
 		String resultsTbl = owlFile.getName();
 		resultsTbl = resultsTbl.substring(0, resultsTbl.lastIndexOf("."));
 		resultsTbl = resultsTbl + "_results";
@@ -41,16 +33,25 @@ public class Main {
 			System.out.println("returned table and results table mismatch!");
 		}
 		System.out.println("results located in: " + returnTbl);
+		File file = new File(".");
+		String currLocation = file.getCanonicalPath();
+		String workingDirectory = null;
+		String OS = (System.getProperty("os.name")).toUpperCase();
+		//to determine what the workingDirectory is.
+		//if it is some version of Windows
+		String pythonLoc = null;
+		if (OS.contains("WIN")){
+			workingDirectory = System.getenv("AppData");
+			pythonLoc = "C:/Python27_64/WinPython-64bit-2.7.9.3/python-2.7.9.amd64/python.exe";
+		} //Otherwise, we assume Linux or Mac
+		else {
+				workingDirectory = System.getProperty("user.home");
+				pythonLoc = "python2";
+			}
+			System.out.println("Executing: " + currLocation);
+			
+	        Process process = new ProcessBuilder(pythonLoc,
+	                    currLocation + "/sql_utils/confusion_matrix.py", returnTbl).start();
 		
-		
-		/*
-		List<String> command = new ArrayList<String>();
-	
-		ProcessBuilder pb = new ProcessBuilder(command);
-		pb.redirectErrorStream(true);
-		Process process = pb.start();
-		
-		"C:/Users/Moran/git/rlp/dataPreprocessingTool/database/confusionMatrix.py"
-		*/
 	}
 }
